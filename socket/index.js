902ca7bd0fa692b6f41b795c0621228dfceb07a4
Nischal <gupta.nischal014@gmail.com>
2022-10-05 10:44:52 +0530

    Starting to build sockets

--- socket/index.js
+++ socket/index.js
@@ -2,14 +2,16 @@ import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 
-const app = express()
-const httpServer = createServer(app)
-const io = new Server(httpServer)
+// const app = express()
+// const httpServer = createServer(app)
+// const io = new Server(httpServer)
 
-app.get("/",(req,res)=>{
-    res.send("<h1>Hello Socket.io</h1>")
+
+// io.on("connection", (socket) => (
+//     console.log("aaya --->",socket)
+// ))
+// httpServer.listen(5000, () => { console.log("server started") })
+
+Server  .on("connection", (socket )=>{
+    console.log("Hello from the server's side")
 })
-io.on("connection", (socket) => (
-    console.log("aaya --->",socket)
-))
-httpServer.listen(5000, () => { console.log("server started") })
