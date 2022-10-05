25ab047062871c71d3b5de67bbc48ed47bb01842
nischalBks <nischal.gupta@bksgold.com>
2022-10-05 19:08:15 +0530

    Haaye Raam! I have to code a lot :(

--- client/src/App.js
+++ client/src/App.js
@@ -15,10 +15,12 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [packet, setPacket] = useState([])
-  const [data, setData] = useState({ d: "" })
+  const [data, setData] = useState({
+    d: ""
+  })
   const socket = io("http://localhost:3001");
-  socket.on("connection", () => {
-    console.log("--->")
+  socket.emit("connection", (socket) => {
+    console.log("--->", socket)
   })
   return (
     <>
@@ -32,6 +34,7 @@ function App() {
           flexDirection: "column"
         }}
       >
+        {/* ============================================================= */}
         <Card
           sx={{
             backgroundColor: "pink",
@@ -47,32 +50,35 @@ function App() {
               <li>
                 {x}
               </li>
+
+
             )}
-
           </ul>
-
         </Card>
+        {/* ============================================================= */}
         <FormGroup>
           <FormControl
             type="text"
             value={packet}
-            placeholder="Type a message here ..."
             autoFocus
           />
           <Input
+            placeholder="Type a message here ..."
             id="x"
-            defaultValue={packet}
-            onChange={(e) => setData({
-              ...data,
-              d: e.target.value
-            })} />
+            onChange={(e) =>
+              setData({
+                ...data,
+                d: e.target.value
+
+              })
+            } />
           <Button
             type="submit"
-            onClick={() =>{
+            onClick={() => {
+              if (document.getElementById("x").value === '') return
               setPacket(packet.concat(data.d))
-              document.getElementById("x").value=''
+              document.getElementById("x").value = ''
             }}
-
           >
             Send
           </Button>
@@ -82,50 +88,4 @@ function App() {
     </>
   );
 }
-
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-// // client/src/pages/chat/send-message.js
-
-// import styles from './styles.module.css';
-// import React, { useState } from 'react';
-
-// const SendMessage = ({ socket, username, room }) => {
-//   const [message, setMessage] = useState('');
-
-//   const sendMessage = () => {
-//     if (message !== '') {
-//       const __createdtime__ = Date.now();
-//       // Send message to server. We can't specify who we send the message to from the frontend. We can only send to server. Server can then send message to rest of users in room
-//       socket.emit('send_message', { username, room, message, __createdtime__ });
-//       setMessage('');
-//     }
-//   };
-
-//   return (
-//     <div className={styles.sendMessageContainer}>
-//       <input
-//         className={styles.messageInput}
-//         placeholder='Message...'
-//         onChange={(e) => setMessage(e.target.value)}
-//         value={message}
-//       />
-//       <button className='btn btn-primary' onClick={sendMessage}>
-//         Send Message
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default SendMessage;
--- socket/index.js
+++ socket/index.js
@@ -2,19 +2,19 @@ import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 
-const io = new Server(5000,{
-    cors:{
-        origin:['http://localhost:3001']
-    }})
-// const app = express()
-// const httpServer = createServer(app)
-// const io = new Server(httpServer)
+// const io = new Server(5000,{
+//     cors:{
+//         origin:['http://localhost:3001']
+//     }})
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer)
 
 
 // io.on("connection", (socket) => (
 //     console.log("aaya --->",socket)
 // ))
-// httpServer.listen(5000, () => { console.log("server started") })
+httpServer.listen(5000, () => { console.log("server started") })
 
 io.on("connection", (socket )=>{
     console.log("Hello from the server's side" + socket.id)
