c40a733f1daf32e575d8cda9f17c662071f479d5
Nischal <gupta.nischal014@gmail.com>
2022-10-05 10:46:28 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,39 @@
-import './App.css';
-import { TextField, Box } from '@mui/material';
-import io from 'socket.io-client'
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { TextField, Box, Card, Typography } from "@mui/material";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 //================================================
-
-const socket = io()
-
+// const socket = io();
 
 function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected)
-  const [lastPong, setLastPong] = useState(null)
+ const[packet, setPacket] = useState("Packet hai")
 
-  useEffect(() => {
-    socket.on("connect", () => {
-      console.log("xxxxxxxxxxxxxx")
-      setIsConnected(true)
-    })
-
-    socket.on("disconnect", () => {
-      setIsConnected(false)
-    })
-
-    socket.on("pong", () => {
-      setLastPong(new Date().toISOString());
-    })
-
-    return () => {
-      socket.off('connect');
-      socket.off('disconnect');
-      socket.off('pong');
-    }
-  }, [])
-
-  const sendPing = () => {
-    socket.emit("ping")
-  }
-
-  return (<>
-    <Box sx={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-
-      <TextField sx={{mr:5}}label="socket.io" />
-      <div>
-      <p>Connected: { '' + isConnected }</p>
-      <p>Last pong: { lastPong || '-' }</p>
-      <button onClick={ sendPing }>Send ping</button>
-    </div>
-    </Box>
-  </>
+  return (
+    <>
+      <Box
+        sx={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection:"column"
+        }}
+      >
+        <Card sx={{
+          backgroundColor:"pink",
+          width:"50%",
+          height:"50%",
+          display:"flex",
+          justifyContent:"center",
+          alignItems:"center"
+        }}>
+          <Typography variant="h4">
+            {packet}
+          </Typography>
+        </Card>
+        <TextField  sx={{ width:"50%",mt:5 }} label="socket.io" />
+      </Box>
+    </>
   );
 }
 
